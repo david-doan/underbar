37fe4e527917cb91ccd14384fb14721989762bd7
david-doan <a.david.doan@gmail.com>
2016-03-03 00:54:10 -0800

    Completed uniq function

--- src/underbar.js
+++ src/underbar.js
@@ -100,6 +100,15 @@
 
   // Produce a duplicate-free version of the array.
   _.uniq = function(array) {
+    var uniques = [];
+
+    _.each(array, function(item){
+      if(_.indexOf(uniques,item) === -1){ 
+        uniques.push(item); return true;
+      } else return false;
+    });
+
+    return uniques;
   };
 
 
