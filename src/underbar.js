fb9dbfcf5010ec98c0fecf33ba99ff95e7d026a0
david-doan <a.david.doan@gmail.com>
2016-04-15 19:02:34 -0700

    completes flatten

--- src/underbar.js
+++ src/underbar.js
@@ -444,6 +444,16 @@
   //
   // Hint: Use Array.isArray to check if something is an array
   _.flatten = function(nestedArray, result) {
+    var answer = result === undefined ? [] : result;
+    
+    return _.reduce(nestedArray, function(accum, value,index){
+      if(!Array.isArray(value)) {
+        accum.push(value);
+        return accum; 
+      } else {
+        return _.flatten(value,accum);
+      }
+    }, answer);
   };
 
   // Takes an arbitrary number of arrays and produces an array that contains
